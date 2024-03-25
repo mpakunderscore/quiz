7e73eb29e16b89328afd0582303fe637a11fcc46
mpakunderscore <mpakunderscore@gmail.com>
2024-03-25 14:41:22 -0700

    Stable

--- init.sh
+++ init.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Navigate to your project directory
-cd ~/quiz
-
-# Start your application with PM2, replace 'app_name' with your actual application name or script
-pm2 start dist/server.js --name server
+#pm2 start dist/server.js --name server
+pm2 stop server
 
 # Pull the latest changes from Git
+cd ~/quiz
 git pull
 
 # Restart your application to apply the latest changes
